afce0eb573c30c71d84e24948c55d7571a59340b
AnicetJonhia <anicet22.aps2a@gmail.com>
2024-10-10 21:36:23 +0300

    pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
   };
 
   return (
-    <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+    <header className="   flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
       <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}> {/* Gérer l'ouverture du Sheet */}
         <SheetTrigger asChild>
           <Button variant="outline" size="icon" className="shrink-0 md:hidden" onClick={() => setIsSheetOpen(true)}>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,7 +7,7 @@ import SidebarNavItem from "./sidebar/SidebarNavItem";
 
 export default function Sidebar() {
   return (
-    <div className="hidden border-r bg-muted/40 md:block">
+    <div className=" hidden border-r bg-muted/40 md:block">
       <div className="flex h-full max-h-screen flex-col gap-2">
         <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
           <Link to={"/"} className="flex items-center gap-2 font-semibold">
