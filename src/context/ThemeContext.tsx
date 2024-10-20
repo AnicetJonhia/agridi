ef2823bc14b7a33d383c412ab10ff3fee24508ee
AnicetJonhia <anicet22.aps2a@gmail.com>
2024-10-20 09:54:56 +0300

    theme

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -23,7 +23,7 @@ const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
 export function ThemeProvider({
   children,
   defaultTheme = "light",
-  storageKey = "vite-ui-theme",
+  storageKey = "agrid-theme",
   ...props
 }: ThemeProviderProps) {
   const [theme, setTheme] = useState<Theme>(
